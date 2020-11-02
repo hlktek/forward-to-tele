8c3e50bd7e485d910a3d88954e5f1d860c07d62f
LevineNguyen98 <vinh.nguyen@ktek.io>
2020-11-02 13:45:14 +0700

    init

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const telegramClient = require('./telegram-client');
 const {HTTP_SERVER_PORT} = require('./config');
-
+const Service = require('./service')
 const app = express();
 
 app.use(bodyParser.json());
@@ -12,6 +12,10 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.post('/telegram', (req, res) => {
     res.status(200).end();
     telegramClient.send(JSON.stringify(req.body));
+})
+app.post('/',async (req, res) => {
+    let response =await Service.send(req.body);
+    res.status(200).send(response.data)
 })
 
 app.listen(HTTP_SERVER_PORT, () => {
---
+++ src/service.js
@@ -0,0 +1,37 @@
+const axios = require('axios');
+const telegram = require('./telegram-client')
+const listIgnore = ['ncloc','ncloc_language_distribution','reliability_rating','security_rating','security_review_rating','sqale_rating']
+async function send(request){
+    let responseObj = {};
+    if(request.project.name){
+        response =await axios.get(`http://178.128.58.75:9009/api/measures/search?projectKeys=${request.project.name}&metricKeys=alert_status%2Cbugs%2Creliability_rating%2Cvulnerabilities%2Csecurity_rating%2Csecurity_hotspots_reviewed%2Csecurity_review_rating%2Ccode_smells%2Csqale_rating%2Cduplicated_lines_density%2Ccoverage%2Cncloc%2Cncloc_language_distribution%2Cprojects`);
+        response.data.measures.map(data=>{
+            if(!listIgnore.includes(data.metric)){
+            responseObj[data.metric] = data.value
+        }
+        })
+        responseObj['project'] = response.data.measures[0].component
+        console.log(JSON.stringify(response.data.measures))
+        let message = '';
+        message += `\n----- Project : ${responseObj.project} -----`
+                    +  "\nBranch:\t" + request.branch.name
+                    +  "\nStatus:\t" + responseObj.alert_status
+                    +  "\nBugs:\t" + responseObj.bugs
+                    +  "\nCode Smells\t" + responseObj.code_smells
+                    +  "\nCoverage:\t" + responseObj.coverage + '%'
+                    +  "\nDuplicated Lines Density:\t" + responseObj.duplicated_lines_density + '%'
+                    +  "\nSecurity Hotspots Reviewed: \t" + responseObj.security_hotspots_reviewed + '%'
+                    +  "\nVulnerabilities: \t" + responseObj.vulnerabilities
+                    +  `\n   ---------   End   ---------   `
+                    
+        telegram.send(message)
+        return {
+            code : 200,
+            data : responseObj
+        }
+    }
+    console.log(request)
+}
+module.exports = {
+    send
+}
