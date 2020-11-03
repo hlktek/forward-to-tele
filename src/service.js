484f6e47d8ee5c5e9d7c6cfa12f2effe6228a383
LevineNguyen98 <vinh.nguyen@ktek.io>
2020-11-03 14:31:12 +0700

    add log when data response

--- src/service.js
+++ src/service.js
@@ -1,17 +1,24 @@
 const axios = require('axios');
 const telegram = require('./telegram-client')
 const config = require('./config')
+const fs = require('fs');
 const listIgnore = ['ncloc','ncloc_language_distribution','reliability_rating','security_rating','security_review_rating','sqale_rating'];
 async function send(request){
     let responseObj = {};
     if(request.project.name){
-        response =await axios.get(`${config.SERVER_HOST}/api/measures/search?projectKeys=${request.project.name}&metricKeys=alert_status%2Cbugs%2Creliability_rating%2Cvulnerabilities%2Csecurity_rating%2Csecurity_hotspots_reviewed%2Csecurity_review_rating%2Ccode_smells%2Csqale_rating%2Cduplicated_lines_density%2Ccoverage%2Cncloc%2Cncloc_language_distribution%2Cprojects`);
+        response =await axios.get(`${config.SERVER_HOST}/api/measures/search?projectKeys=${request.project.key}&metricKeys=alert_status%2Cbugs%2Creliability_rating%2Cvulnerabilities%2Csecurity_rating%2Csecurity_hotspots_reviewed%2Csecurity_review_rating%2Ccode_smells%2Csqale_rating%2Cduplicated_lines_density%2Ccoverage%2Cncloc%2Cncloc_language_distribution%2Cprojects`);
+        let logObj = {time: request.analysedAt, response: response.data}
+        fs.appendFile('log.txt', JSON.stringify(logObj)+'\n', function (err) {
+            if (err) throw err;
+            console.log('log!');
+          });
         response.data.measures.map(data=>{
             if(!listIgnore.includes(data.metric)){
             responseObj[data.metric] = data.value;
         }
         })
-        responseObj['project'] = response.data.measures[0].component;
+
+        responseObj['project'] = response.data.measures.length >0 ? response.data.measures[0].component : '?????';
         let message = '';
         message += `\n----- Project : ${responseObj.project} -----`
                     +  "\nBranch:\t" + request.branch.name
@@ -23,7 +30,7 @@ async function send(request){
                     +  "\nSecurity Hotspots Reviewed: \t" + responseObj.security_hotspots_reviewed + '%'
                     +  "\nVulnerabilities: \t" + responseObj.vulnerabilities
                     +  `\n   ---------   End   ---------   `
-                    
+        
         telegram.send(message);
         return {
             code : 200,
