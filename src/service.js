c2f09e1db551f52fa9baf590fbed975a476e3734
LevineNguyen98 <vinh.nguyen@ktek.io>
2021-06-01 18:07:25 +0700

    replace doamin

--- src/service.js
+++ src/service.js
@@ -17,11 +17,12 @@ async function send(request){
             responseObj[data.metric] = data.value;
         }
         })
-
+        const replaceIp = "http://188.166.210.246:9009";
+        const replaceDomain = "https://sonar.gmstd.dev";
         responseObj['project'] = response.data.measures.length >0 ? response.data.measures[0].component : '?????';
         let message = '';
         message += `\n----- Project : ${responseObj.project} -----`
-                    +  "\nUrl:\t" + request.branch.url
+                    +  "\nUrl:\t" + request.branch.url.includes(replaceIp) ? request.branch.url.replace(replaceIp,replaceDomain) : request.branch.url
                     +  "\nBranch:\t" + request.branch.name
                     +  "\nStatus:\t" + responseObj.alert_status
                     +  "\nBugs:\t" + responseObj.bugs
